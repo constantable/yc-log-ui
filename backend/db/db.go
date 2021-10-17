47f2b2b824bd8248cd13b92b811e44901927f2c3
Konstantin Chigakov <itc.masters@gmail.com>
2021-10-17 06:53:08 +0400

    Users' handler implemented

--- backend/db/db.go
+++ backend/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -15,6 +16,9 @@ WHERE username = ? and password = ?`
 
 	getUserSQL = `
 SELECT id, username, isadmin FROM users WHERE username = ? LIMIT 1
+`
+	getUserByIdSQL = `
+SELECT id, username, isadmin FROM users WHERE id = ? LIMIT 1
 `
 	newRefreshToken = `
 INSERT INTO refresh_tokens (userid, tokents) VALUES (?,?) ON CONFLICT(userid) DO UPDATE SET tokents = ?;
@@ -42,6 +46,19 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     userid INTEGER UNIQUE,
     tokents BIGINT
 );
+`
+	createUser = `
+INSERT INTO users (username, password, isadmin) VALUES(?, ?, ?);
+`
+	removeUser = `
+DELETE FROM users WHERE id= ?;
+`
+	usersSQL = `
+SELECT id, username, isadmin FROM users;
+`
+	updateUser = `
+UPDATE users SET password = ?, isadmin = ?
+ WHERE id = ?;
 `
 )
 
@@ -111,6 +128,20 @@ func (db *DB) GetUser(username string) (usr User, err error) {
 	return
 }
 
+func (db *DB) GetUserById(id uint64) (usr User, err error) {
+	stmt, err := db.sql.Prepare(getUserByIdSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(id).Scan(&usr.Id, &usr.Username, &usr.IsAdmin)
+	if err != nil {
+		log.Println(err)
+	}
+	return
+}
+
 func (db *DB) NewRefreshToken(uid uint64, tokents int64) error {
 	tx, err := db.sql.Begin()
 	if err != nil {
@@ -174,6 +205,108 @@ func (db *DB) SetPassword(username string, password string) (usr User, err error
 		return
 	}
 	usr, err = db.GetUser(username)
+
+	return
+}
+
+func (db *DB) CreateUser(username string, password string, isadmin bool) (usr User, err error) {
+	tx, err := db.sql.Begin()
+	if err != nil {
+		return
+	}
+	stmt, err := db.sql.Prepare(createUser)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = tx.Stmt(stmt).Exec(username, password, isadmin)
+	if err != nil {
+		err1 := tx.Rollback()
+		if err1 != nil {
+			return
+		}
+		return
+	}
+	err = tx.Commit()
+	if err != nil {
+		return
+	}
+	usr, err = db.GetUser(username)
+
+	return
+}
+
+func (db *DB) UpdateUser(id uint64, password string, isadmin bool) (usr User, err error) {
+	tx, err := db.sql.Begin()
+	if err != nil {
+		return
+	}
+	stmt, err := db.sql.Prepare(updateUser)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = tx.Stmt(stmt).Exec(password, isadmin, id)
+	if err != nil {
+		err1 := tx.Rollback()
+		if err1 != nil {
+			return
+		}
+		return
+	}
+	err = tx.Commit()
+	if err != nil {
+		return
+	}
+	usr, err = db.GetUserById(id)
+
+	return
+}
+
+func (db *DB) GetUsers() (users []User, err error) {
+	rows, err := db.sql.Query(usersSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		p := User{}
+		err := rows.Scan(&p.Id, &p.Username, &p.IsAdmin)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		users = append(users, p)
+	}
+	return
+}
+
+func (db *DB) RemoveUser(id uint64) (err error) {
+	tx, err := db.sql.Begin()
+	if err != nil {
+		return
+	}
+	stmt, err := db.sql.Prepare(removeUser)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = tx.Stmt(stmt).Exec(id)
+	if err != nil {
+		err1 := tx.Rollback()
+		if err1 != nil {
+			return
+		}
+		return
+	}
+	err = tx.Commit()
+	if err != nil {
+		return
+	}
 
 	return
 }
--- backend/handlers/users.go
+++ backend/handlers/users.go
@@ -3,25 +3,89 @@ package handlers
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"strconv"
 )
 
+type User struct {
+	Id       uint64 `json:"id,omitempty"`
+	Username string `json:"username"`
+	Password string `json:"password,omitempty"`
+	IsAdmin  bool   `json:"isAdmin"`
+}
+
 func (h *Handlers) CreateUser(c echo.Context) error {
-	return c.String(http.StatusOK, "Not implemented")
+	userReq := User{}
+
+	if err := c.Bind(&userReq); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	if len(userReq.Username) < 3 {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"message": "Username should be more than 3 chars length",
+		})
+	}
+	if len(userReq.Password) < 3 {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"message": "Password should be more than 3 chars length",
+		})
+	}
+
+	user, err := dbHandle.CreateUser(userReq.Username, userReq.Password, userReq.IsAdmin)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	return c.JSON(http.StatusOK, User(user))
 }
 
 func (h *Handlers) GetUser(c echo.Context) error {
-	//id, _ := strconv.Atoi(c.Param("id"))
-	return c.String(http.StatusOK, "Not implemented")
+	id, _ := strconv.Atoi(c.Param("id"))
+	user, err := dbHandle.GetUserById(uint64(id))
+	if err != nil {
+		return c.JSON(http.StatusNotFound, err)
+	}
+	return c.JSON(http.StatusOK, User(user))
 }
 
 func (h *Handlers) UpdateUser(c echo.Context) error {
-	return c.String(http.StatusOK, "Not implemented")
+	id, _ := strconv.Atoi(c.Param("id"))
+	if id == 1 {
+		return c.JSON(http.StatusForbidden, map[string]string{
+			"message": "Can't update root",
+		})
+	}
+	userReq := User{}
+	if err := c.Bind(&userReq); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	user, err := dbHandle.UpdateUser(uint64(id), userReq.Password, userReq.IsAdmin)
+	if err != nil {
+		return c.JSON(http.StatusNotFound, err)
+	}
+	return c.JSON(http.StatusOK, User(user))
 }
 
 func (h *Handlers) DeleteUser(c echo.Context) error {
-	return c.String(http.StatusOK, "Not implemented")
+	id, _ := strconv.Atoi(c.Param("id"))
+	if id == 1 {
+		return c.JSON(http.StatusForbidden, map[string]string{
+			"message": "Can't delete root",
+		})
+	}
+	err := dbHandle.RemoveUser(uint64(id))
+	if err != nil {
+		return c.JSON(http.StatusNotFound, err)
+	}
+	return c.JSON(http.StatusOK, nil)
 }
 
 func (h *Handlers) GetAllUsers(c echo.Context) error {
-	return c.String(http.StatusOK, "Not implemented")
+	users, err := dbHandle.GetUsers()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	var ret []User
+	for _, user := range users {
+		ret = append(ret, User(user))
+	}
+	return c.JSON(http.StatusOK, ret)
 }
